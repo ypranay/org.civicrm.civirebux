74ca0b4953f90b0e43154e7b393c98cc3cd50f88
ypranay <y.pranay@gmail.com>
2016-06-18 07:23:35 -0500

    bug fix - navigation item

--- CRM/Civirebux/Page/ContribReport.php
+++ CRM/Civirebux/Page/ContribReport.php
@@ -5,10 +5,12 @@ require_once 'CRM/Core/Page.php';
 class CRM_Civirebux_Page_ContribReport extends CRM_Core_Page {
   public function run() {
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
-    CRM_Utils_System::setTitle(ts('ContribReport'));
+    CRM_Utils_System::setTitle(ts('CiviREBUX'));
 
-    // Example: Assign a variable for use in a template
-    $this->assign('currentTime', date('Y-m-d H:i:s'));
+// Example: Assign a variable for use in a template
+    $this->assign('contribData', json_encode(CRM_Civirebux_Data::get())); 
+    $session = CRM_Core_Session::singleton();
+    $this->assign('loggedin_user',$session->get('userID'));    
 
     parent::run();
   }
