a8c2659aa3b083434091ccd706966e2cc1022fe6
ypranay <y.pranay@gmail.com>
2016-06-20 01:42:26 -0500

    get contrib data method added

--- CRM/Civirebux/Page/ContribReport.php
+++ CRM/Civirebux/Page/ContribReport.php
@@ -7,10 +7,7 @@ class CRM_Civirebux_Page_ContribReport extends CRM_Core_Page {
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     CRM_Utils_System::setTitle(ts('CiviREBUX'));
 
-// Example: Assign a variable for use in a template
     $this->assign('contribData', json_encode(CRM_Civirebux_Data::get())); 
-    //$session = CRM_Core_Session::singleton();
-    //$this->assign('loggedin_user',$session->get('userID'));    
 
     parent::run();
   }
