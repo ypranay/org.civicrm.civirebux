7a7f95b6f16b1190c5b27b2bd10e75d0e7cef0e7
ypranay <y.pranay@gmail.com>
2016-06-27 05:19:51 -0500

    Added Membership methods

--- CRM/Civirebux/Data.php
+++ CRM/Civirebux/Data.php
@@ -12,9 +12,9 @@ class CRM_Civirebux_Data {
 	 * 
 	 * @return array
 	 */
-	public static function get() {
+	public static function getContributionData() {
 		self::$fields = self::getContributionFields();
-		self::$emptyRow = self::getEmptyRow();
+		self::$emptyRow = self::getEmptyRow(true);
 		self::$multiValues = array();
 		$contributions = civicrm_api3('Contribution', 'get', array(
 				'sequential' => 1,
@@ -22,14 +22,32 @@ class CRM_Civirebux_Data {
       				'return' => implode(',',array_keys(self::$fields)),
       				'options' => array('sort' => 'id ASC', 'limit' => 0)
 			));
-		return self::splitMultiValues(self::formatResult($contributions['values']));
+		return self::splitMultiValues(self::formatContributionResult($contributions['values']));
 	}
+
+	 /**
+         * Return an array containing formatted Membership data.
+         *
+         * @return array
+         */
+        public static function getMembershipData() {
+                self::$fields = self::getMembershipFields();
+                self::$emptyRow = self::getEmptyRow(false);
+                self::$multiValues = array();
+                $membershipinfo = civicrm_api3('Membership', 'get', array(
+                                'sequential' => 1,
+                                'api.Membership.get' => array(),
+                                'return' => implode(',',array_keys(self::$fields)),
+                                'options' => array('sort' => 'id ASC', 'limit' => 0)
+                        ));
+                return self::splitMultiValues(self::formatMembershipResult($membershipinfo['values']));
+        }
 
 	/**
 	 * Return an array containing $data rows and each row containing multiple values of at least one field is populated into separate row for each field's
 	 * multiple value.
 	 * 
-	 * @param array   $data       array containing a set of Contributions
+	 * @param array   $data       array containing a set of Contributions/Membership data
 	 * 
 	 * @return array
 	 */
@@ -49,8 +67,8 @@ class CRM_Civirebux_Data {
 	/**
 	 * Return an array containing set of rows which are built basing on given $row  and $fields array with indexes of multi values of the $row.
 	 * 
-	 * @param array   $row        a single Contribution row
-	 * @param array   $fields     array containing Contribution multi value fields as keys and integer indexes as values
+	 * @param array   $row        a single record
+	 * @param array   $fields     array containing contribution/membership multi value fields as keys and integer indexes as values
 	 * 
 	 * @return array
 	 */
@@ -86,7 +104,7 @@ class CRM_Civirebux_Data {
 	 * 
 	 * @return type
 	 */
-	protected static function formatResult($data, $dataKey = null, $level = 0) {
+	protected static function formatContributionResult($data, $dataKey = null, $level = 0) {
 		$result = array();
 		if ($level < 2) {
 			if ($level === 1) {
@@ -104,7 +122,7 @@ class CRM_Civirebux_Data {
 				if (!empty(self::$fields[$key]['title'])) {
 					$key = self::$fields[$key]['title'];
 				}
-				$result[$key] = self::formatResult($value, $dataKey, $level + 1);
+				$result[$key] = self::formatContributionResult($value, $dataKey, $level + 1);
 				if ($level === 1 && is_array($result[$key])) {
 					self::$multiValues[$baseKey][] = $key;
 				}
@@ -114,6 +132,44 @@ class CRM_Civirebux_Data {
 		}
 		return $result;
 	}	
+
+   	/**
+         * Return a result of recursively parsed and formatted $data.
+         *
+         * @param mixed   $data       data element
+         * @param string  $dataKey    key of current $data item
+         * @param int     $level      how deep we are relative to the root of our data
+         *
+         * @return type
+         */
+        protected static function formatMembershipResult($data, $dataKey = null, $level = 0) {
+                $result = array();
+                if ($level < 2) {
+                        if ($level === 1) {
+                                $result = self::$emptyRow;
+                        }
+                        $baseKey = $dataKey;
+                        foreach ($data as $key => $value) {
+                                if (empty(self::$fields[$key]) && $level) {
+                                        continue;
+                                }
+                                if ($level === 0 && empty($value['api.Membership.get']['values'])) {
+                                        continue;
+                                }
+                                $dataKey = $key;
+                                if (!empty(self::$fields[$key]['title'])) {
+                                        $key = self::$fields[$key]['title'];
+                                }
+                                $result[$key] = self::formatMembershipResult($value, $dataKey, $level + 1);
+                                if ($level === 1 && is_array($result[$key])) {
+                                        self::$multiValues[$baseKey][] = $key;
+                                }
+                        }
+                } else {
+                        return self::formatValue($dataKey, $data);
+                }
+                return $result;
+        }	
 
 	/**
 	 * Return $value formatted by available Option Values for the $key Field. If there is no Option Values for the field, then return $value itself with HTML tags stripped.
@@ -156,19 +212,59 @@ class CRM_Civirebux_Data {
 		if (!empty(self::$fields[$key]['optionValues'])) {
 			return self::$fields[$key]['optionValues'][$value];
 		}
-		return strip_tags($value);
+		return strip_tags(self::customizeValue($key, $value));
 	}
-	
 
-	protected static function getEmptyRow() {
-		$result = array();
-		foreach (self::$fields as $key => $value) {
-			if (!empty($value['contribution_id'])) {
-				$key = $value['contribution_id'];
-			}
-			$result[$key] = '';
+
+	/**
+	 * Additional function for customizing Membership value by its key
+	 * (if it's needed). For example: we want to return Campaign's title
+	 * instead of ID.
+	 * 
+	 * @param string $key
+	 * @param string $value
+	 * @return string
+	 */
+	protected static function customizeValue($key, $value) {
+		$result = $value;
+		switch ($key) {
+			case 'campaign_id':
+				if (!empty($value)) {
+					$campaign = civicrm_api3('Campaign', 'getsingle', array(
+								'sequential' => 1,
+								'return' => "title",
+								'id' => $value,
+								));
+					if ($campaign['is_error']) {
+						$result = '';
+					} else {
+						$result = $campaign['title'];
+					}
+				}
+				break;
 		}
+		return $result;
+	}	
 
+
+	protected static function getEmptyRow($isContribution) {
+		$result = array();
+		if($isContribution){
+			foreach (self::$fields as $key => $value) {
+				if (!empty($value['contribution_id'])) {
+					$key = $value['contribution_id'];
+				}
+				$result[$key] = '';
+			}
+		}
+		else{
+			foreach (self::$fields as $key => $value) {
+                                if (!empty($value['membership_type_id'])) {
+                                        $key = $value['membership_type_id'];
+                                }
+                                $result[$key] = '';
+                        }
+		}
 		return $result;
 	}
 
@@ -213,6 +309,7 @@ class CRM_Civirebux_Data {
 		$fields['display_name'] = array('name' => 'display_name', 'title' => 'Display Name');
 		$fields['sort_name'] = array('name' => 'sort_name', 'title' => 'Sort Name');
 		$fields['contact_type'] = array('name' => 'contact_type', 'title' => 'Contact Type');
+		$fields['id'] = array('name' => 'id', 'title' => 'ID');		
 		
 		foreach ($fields as $key => $value) {
 			$key = $value['name'];
@@ -220,4 +317,47 @@ class CRM_Civirebux_Data {
 		}
 		return $result;
 	}
+
+	/**
+         * Return an array containing all Fields and Custom Fields of Membership entity, keyed by their API keys and extended with available fields Option Values.
+         * @return array
+         */
+	protected static function getMembershipFields() {
+                $fields = CRM_Member_DAO_Membership::fields();
+		$keys = CRM_Member_DAO_Membership::fieldKeys();
+                $result = array();
+
+                $customFieldsResult = CRM_Core_DAO::executeQuery(
+                                'SELECT g.id AS group_id, f.id AS id, f.label AS label, f.data_type AS data_type, ' .
+                                'f.html_type AS html_type, f.date_format AS date_format, og.name AS option_group_name ' .
+                                'FROM `civicrm_custom_group` g ' .
+                                'LEFT JOIN `civicrm_custom_field` f ON f.custom_group_id = g.id ' .
+                                'LEFT JOIN `civicrm_option_group` og ON og.id = f.option_group_id ' .
+                                'WHERE g.extends = \'Membership\' AND g.is_active = 1 AND f.is_active = 1'
+                                );
+                while ($customFieldsResult->fetch()) {
+                        $customField = new CRM_Core_BAO_CustomField();
+                        $customField->id = $customFieldsResult->id;
+                        $customField->find(true);
+                        $fields['custom_' . $customFieldsResult->id] = array(
+                                        'name' => 'custom_' . $customFieldsResult->id,
+                                        'title' => $customFieldsResult->label,
+                                        'pseudoconstant' => array(
+                                                'optionGroupName' => $customFieldsResult->option_group_name,
+                                                ),
+                                        'customField' => (array)$customField
+                                        );
+                }
+		
+		$fields['membership_name'] = array('name' => 'membership_name', 'title' => 'Membership Name');
+                $fields['relationship_name'] = array('name' => 'relationship_name', 'title' => 'Relationship Name');
+                $fields['id'] = array('name' => 'id', 'title' => 'ID');
+		$fields['status_id'] = array('name' => 'status_id', 'title' => 'Status_ID');        	
+		$fields['is_pay_later'] = array('name' => 'is_pay_later', 'title' => 'Is_Pay_Later');        
+		foreach ($fields as $key => $value) {
+                        $key = $value['name'];
+                        $result[$key] = $value;
+                }
+                return $result;
+        }
 }
