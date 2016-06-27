d63ebff7446b431cb16e4aa8845fba1fa36d968b
ypranay <y.pranay@gmail.com>
2016-06-27 05:39:20 -0500

    filtered membership attribs

--- CRM/Civirebux/Page/ContribReport.php
+++ CRM/Civirebux/Page/ContribReport.php
@@ -22,6 +22,41 @@ class CRM_Civirebux_Page_ContribReport extends CRM_Core_Page {
 	}
 	return $newcontribdata;
   }
+ 
+/*
+  public function filterMembershipAttribs($data){
+        $newcontribdata = array();
+        foreach($contribdata as $data) {
+                $newdata = array();
+                foreach(array_keys($data) as $key){
+                        if($key == 'campaign_id' || $key == 'contribution_recur_id' || $key == 'is_override' or $key == 'max_related' or $key == 'owner_membership_id'){
+				$newdata[$key] = $data[$key];
+			}
+			elseif(!ctype_lower($key[0])){
+				$newdata[$key] = $data[$key];
+			}
+                }
+		foreach($newdata as $key){
+			echo $key;
+		}
+                array_push($newcontribdata,$newdata);
+        }
+        return $newcontribdata;
+  } */
+
+  public function filterMembershipAttribs($contribdata){
+        $newcontribdata = array();
+        foreach($contribdata as $data) {
+                $newdata = array();
+                foreach(array_keys($data) as $key){
+                        if(!ctype_lower($key[0])){
+                                $newdata[$key] = $data[$key];
+                        }
+                }
+                array_push($newcontribdata,$newdata);
+        }
+        return $newcontribdata;
+  } 
   
   public function run() {
     	CRM_Utils_System::setTitle(ts('CiviREBUX: Report Building Extension'));
@@ -30,7 +65,7 @@ class CRM_Civirebux_Page_ContribReport extends CRM_Core_Page {
 		$this->assign('pivotData', json_encode(self::filterContributionAttribs(CRM_Civirebux_Data::getContributionData())));
 	}
 	else{
-		$this->assign('pivotData', json_encode(CRM_Civirebux_Data::getMembershipData()));
+		$this->assign('pivotData', json_encode(self::filterMembershipAttribs(CRM_Civirebux_Data::getMembershipData())));
 	}
 	$this->assign('CRMDataType',$CRMDataType);  
 	$options_array = array('Contribution' => 'Contribution','Membership' => 'Membership');
