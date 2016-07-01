412e0833dca880a6c3d4557f1beb87c01a5f0cb0
ypranay <y.pranay@gmail.com>
2016-07-01 09:05:03 -0500

    new timestamp

--- js/pivottable/export_renderers.js
+++ js/pivottable/export_renderers.js
@@ -1,16 +1,8 @@
 function getcurrenttimestamp() {
 	var now = new Date();
-       	var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
-      	var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
-        var suffix = ( time[0] < 12 ) ? "AM" : "PM";
-       	time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
-       	time[0] = time[0] || 12;
-       	for ( var i = 1; i < 3; i++ ) {
-       		if ( time[i] < 10 ) {
-               		time[i] = "0" + time[i];
-                }
-       	}
-   	return date.join("/")+"_"+time.join(":")+suffix;
+       	var date = [ now.getFullYear(), now.getMonth()+1, now.getDate() ];
+      	var time = [ now.getHours(), now.getMinutes() ];
+   	return date.join('')+"_"+time.join('');
 }
 
 (function() {
