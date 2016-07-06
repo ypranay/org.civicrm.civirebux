06ade0cf6d2aa4ef41b40acbb9f0bac309d60499
ypranay <y.pranay@gmail.com>
2016-07-06 01:22:18 -0500

    action for AJAX save call

--- CRM/Civirebux/BAO/SaveReport.php
+++ CRM/Civirebux/BAO/SaveReport.php
@@ -3,4 +3,18 @@ class CRM_Civirebux_BAO_SaveReport{
 	/**
 	* Handle AJAX request to save report configuration 
 	*/
+	static function save($data){
+	/*	foreach($data as $key=>$value){
+			echo $key.":->".$value."<br>";
+		}*/
+		if(isset($_REQUEST['save'])){
+			$config = $_POST['save'];
+			$res = 'got it';
+			die;
+		}
+		else{
+			$res = 'nope!!';
+			die;
+		}
+	}
 }
