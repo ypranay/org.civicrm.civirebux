3a97a3e7fb0017966bf9c7d3abd95d2d790b3b0d
ypranay <y.pranay@gmail.com>
2016-08-10 09:21:02 -0500

    removed upgrade_() and fixed install()

--- CRM/Civirebux/Upgrader.php
+++ CRM/Civirebux/Upgrader.php
@@ -6,28 +6,6 @@ class CRM_Civirebux_Upgrader extends CRM_Civirebux_Upgrader_Base {
    * @return boolean
    */
   public function install() {
-    $this->upgrade_();
-    return TRUE;
-  }
-
-  /**
-   * While uninstalling
-   * 
-   * @return boolean
-   */
-  public function uninstall()
-  {
-    CRM_Core_DAO::executeQuery("DELETE FROM `civicrm_navigation` WHERE name = 'civirebux'");
-    CRM_Core_BAO_Navigation::resetNavigation();
-    return TRUE;
-  }
-
-  /**
-   * Install Civirebux link under Reports menu.
-   * 
-   * @return boolean
-   */
-  public function upgrade_() {
     CRM_Core_DAO::executeQuery("DELETE FROM `civicrm_navigation` WHERE name = 'civirebux' and parent_id IS NULL");
     $reportsNavId = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_Navigation', 'Reports', 'id', 'name');
     $navigation = new CRM_Core_DAO_Navigation();
@@ -41,10 +19,40 @@ class CRM_Civirebux_Upgrader extends CRM_Civirebux_Upgrader_Base {
         'permission' => 'access CiviCRM Civirebux',
         'separator'  => 1,
         'is_active'  => 1
-    );
+        );
     $navigation->copyValues($params);
     $navigation->save();
     CRM_Core_BAO_Navigation::resetNavigation();
+
+    CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS `civicrm_civirebux_configuration`");
+
+    CRM_Core_DAO::executeQuery("CREATE TABLE `civicrm_civirebux_configuration` (
+        `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+        `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+        `renderer` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+        `aggregator` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+        `vals` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+        `rows` varchar(512) COLLATE utf8_unicode_ci DEFAULT NULL,
+        `cols` varchar(512) COLLATE utf8_unicode_ci DEFAULT NULL,
+        `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        `desc` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+        `type` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+        PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=99 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci");
+
+    return TRUE;
+  }
+
+  /**
+   * While uninstalling
+   * 
+   * @return boolean
+   */
+  public function uninstall()
+  {
+    CRM_Core_DAO::executeQuery("DELETE FROM `civicrm_navigation` WHERE name = 'civirebux'");
+    CRM_Core_BAO_Navigation::resetNavigation();
+    CRM_Core_DAO::executeQuery("DROP TABLE IF EXISTS `civicrm_civirebux_configuration`");
     return TRUE;
   }
 
